8c96c30bae190406e631583a975a04891470c955
Ankurjainaj <42439998+Ankurjainaj@users.noreply.github.com>
2019-10-03 22:00:40 +0530

    Updated the python file
    Changed the variable names for array name and i for a

--- Prerequisites/PRE4.py
+++ Prerequisites/PRE4.py
@@ -1,6 +1,6 @@
 t = int(input())
-ar = list(map(int,input().split()))
-for a in range(t//2):
-  sum = ar[a]+ar[-a-1]
+arr = list(map(int,input().split()))
+for i in range(t//2):
+  sum = arr[i]+arr[-i-1]
   print(sum//10,sum%10)
     
